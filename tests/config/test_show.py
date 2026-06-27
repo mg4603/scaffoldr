25956d2aaeb628525fac4cfc9f147c82e691f031
Michael George <michael.g.george@gmail.com>
2026-06-27 16:11:11 +0530

    refactor: use make_mock_config fixture for config show cmd tests

--- tests/config/test_show.py
+++ tests/config/test_show.py
@@ -1,5 +1,3 @@
-from unittest.mock import MagicMock, patch
-
 from typer.testing import CliRunner
 
 from scaffoldr.main import app
@@ -7,31 +5,19 @@ from scaffoldr.main import app
 runner = CliRunner()
 
 
-def test_show_success(tmp_path):
-    mock_config = MagicMock()
-    mock_config.author = "test_author"
-    mock_config.github_username = "test_username"
-    mock_config.python_version = "test_version"
-    mock_config.license = "test_license"
-    mock_config.default_private = False
-    mock_config.required_reviewers = 1
-    mock_config.use_ssh = True
-    mock_config.github_token = ""
+def test_show_success(tmp_path, make_mock_config):
+    _ = make_mock_config("scaffoldr.config.show.Config.load")
 
-    with patch(
-        "scaffoldr.config.show.Config.load",
-        return_value=mock_config,
-    ):
-        result = runner.invoke(app, ["config", "show"])
+    result = runner.invoke(app, ["config", "show"])
 
-        assert result.exit_code == 0
-        assert (
-            "author             = 'test_author'\n"
-            "github_username    = 'test_username'\n"
-            "python_version     = 'test_version'\n"
-            "license            = 'test_license'\n"
-            "default_private    = False\n"
-            "required_reviewers = 1\n"
-            "use_ssh            = True\n"
-            "github_token       = (not set)"
-        ) in result.output
+    assert result.exit_code == 0
+    assert (
+        "author             = 'test_author'\n"
+        "github_username    = 'user'\n"
+        "python_version     = 'test_version'\n"
+        "license            = 'test_license'\n"
+        "default_private    = True\n"
+        "required_reviewers = 1\n"
+        "use_ssh            = True\n"
+        "github_token       = toke****\n"
+    ) in result.output
